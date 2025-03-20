13336fcc0a9f8cd982ae8f617a45f8ddeafea661
mohamedDev69 <mohamedchettah0208@gmail.com>
2025-03-20 22:56:14 +0100

    feat: wip scrap title

--- backend/app/Http/Controllers/Extension/WebsiteController.php
+++ backend/app/Http/Controllers/Extension/WebsiteController.php
@@ -70,4 +70,41 @@ class WebsiteController
         }
     }
 
+    private function getFromHtml(string $html, array $tags) {
+
+        $collect = [];
+
+        // Turn off default error reporting so we're not drowning
+        // in errors when the HTML is malformed. We can get a
+        // hold of them anytime via libxml_get_errors().
+        // Cf. https://www.php.net/libxml_use_internal_errors
+        libxml_use_internal_errors(true);
+
+        // Turn HTML string into a DOM tree.
+        $dom = new DomDocument;
+        $dom->loadHTML($html);
+
+        // Set up XPath
+        $xpath = new DomXPath($dom);
+
+        // Query the DOM tree for the given set of tags.
+        foreach ($tags as $tag) {
+
+            // You can do *a lot* more with XPath, cf. this cheat sheet:
+            // https://gist.github.com/LeCoupa/8c305ec8c713aad07b14
+            $result = $xpath->query("//{$tag}");
+
+            if ($result instanceof DOMNodeList) {
+
+                $collect[$tag] = $result;
+            }
+        }
+
+        // Clear errors to free up memory, cf.
+        // https://www.php.net/manual/de/function.libxml-use-internal-errors.php#78236
+        libxml_clear_errors();
+
+        return $collect;
+    }
+
 }
