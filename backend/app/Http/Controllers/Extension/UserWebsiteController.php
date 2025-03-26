eb824226bb98079e68b318603f1fb7b6435e4d3d
mohamedDev69 <mohamedchettah0208@gmail.com>
2025-03-26 18:05:26 +0100

    fix: remove the scrap on the page

--- backend/app/Http/Controllers/Extension/UserWebsiteController.php
+++ backend/app/Http/Controllers/Extension/UserWebsiteController.php
@@ -22,7 +22,7 @@ class UserWebsiteController
     public function store(StoreWebsiteRequest $request): JsonResponse
     {
         $validated = $request->validated();
-        $websiteInfo = $this->fetchTitleAndFavicon($validated['website_url']);
+        $websiteInfo = $this->fetchFavicon($validated['website_url']);
 
         $websiteInfo['website_name'] = ucfirst($validated['title']);
 
@@ -53,45 +53,11 @@ class UserWebsiteController
         }
     }
 
-    private function fetchTitleAndFavicon($url): array
+    private function fetchFavicon($url): array
     {
         try {
             // Récupération du favicon
             $favicon = Http::get('https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=' . $url . '&size=16')->headers()['Content-Location'][0];
-
-            // Récupération du HTML de la page
-            $response = Http::get($url);
-            $html = $response->body();
-
-            // check if the url is valid
-            if ($response->status() !== 200) {
-                return [
-                    'website_url'     => $url,
-                    'favicon' => $favicon,
-                    'error'   => 'Impossible de récupérer le contenu de la page.'
-                ];
-            }
-
-            if (empty($html)) {
-                return [
-                    'website_url'     => $url,
-                    'website_name'   => null,
-                    'favicon' => $favicon,
-                    'error'   => 'Impossible de récupérer le contenu de la page.'
-                ];
-            }
-
-            // Charger le HTML dans DOMDocument
-            libxml_use_internal_errors(true);
-            $dom = new DOMDocument();
-            $dom->loadHTML(mb_convert_encoding($html, 'HTML-ENTITIES', 'UTF-8'));
-            libxml_clear_errors();
-
-            // Utiliser DOMXPath pour récupérer le titre
-            $xpath = new DOMXPath($dom);
-            $titleNodes = $xpath->query('//title');
-
-            $title = $titleNodes->length > 0 ? trim($titleNodes->item(0)->nodeValue) : null;
 
             return [
                 'website_url'     => $url,
