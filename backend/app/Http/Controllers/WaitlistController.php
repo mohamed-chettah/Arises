ae5e59d55c2b099ca8a3d5088e9b87566eaebc75
mohamedDev69 <mohamedchettah0208@gmail.com>
2025-04-08 22:51:50 +0200

    feat: sentry

--- backend/app/Http/Controllers/WaitlistController.php
+++ backend/app/Http/Controllers/WaitlistController.php
@@ -13,11 +13,12 @@ class WaitlistController
         $validated = $request->validated();
 
         try {
-             WaitlistService:: create($validated);
+             WaitlistService::create($validated);
 
             return response()->json("Ajout à la liste d'attente réussi", 201);
         }
         catch (\Throwable $e) {
+            \Sentry\captureException($e); // ← Ajout important ici
             return response()->json("Erreur lors de l'ajout à la liste d'attente", 500);
         }
     }
