bf0dfb5c32b7b37d4e7ac5bb5163da5d8bfce9de
mohamedDev69 <mohamedchettah0208@gmail.com>
2025-04-08 22:58:17 +0200

    feat: rename waitlist to waitlists

--- backend/database/migrations/2025_04_08_090929_create_waitlist.php
+++ backend/database/migrations/2025_04_08_090929_create_waitlist.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('waitlist', function (Blueprint $table) {
+        Schema::create('waitlists', function (Blueprint $table) {
             $table->id();
             $table->string('email', 70)->unique();
             $table->string('name', 50)->nullable();
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('user_websites');
+        Schema::dropIfExists('waitlists');
     }
 };
