513bb40be1e90a217fc56978a523825053731e5f
mohamedDev69 <mohamedchettah0208@gmail.com>
2025-04-13 17:03:46 +0200

    feat: deployment simplify

--- backend/app/Services/WebsiteService.php
+++ backend/app/Services/WebsiteService.php
@@ -17,7 +17,7 @@ class WebsiteService
 
     public static function findOrCreate(array $data)
     {
-        $website = Website::where('website_url', $data['website_url'])->first();
+        $website = Website::whereIn('website_url', $data['website_url'])->first();
 
         if ($website) {
             return $website;
